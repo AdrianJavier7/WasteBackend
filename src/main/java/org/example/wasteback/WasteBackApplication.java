0164f9eb82c3a9a91b4c4a8325c19c87aa700420
adrian <aperezmiranda@safareyes.es>
2024-10-31 12:46:12 +0100

    a empezar

---
+++ src/main/java/org/example/wasteback/WasteBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.wasteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/wasteback/WasteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wasteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
